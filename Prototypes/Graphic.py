d4a23149b93fc8269cfccfe136e812de53f5cf2f
JDR-3141 <158481014+JDR-3141@users.noreply.github.com>
2024-03-22 16:17:52 +0000

    edit

--- Prototypes/Graphic.py
+++ Prototypes/Graphic.py
@@ -1,28 +1,28 @@
 import tkinter as tk
 
-class Square:
-    
-    def __init__(self, x, y, state, colour):
-        self.x = x
-        self.y = y
-        self.state = state
-        self.colour = colour
-        self.image = canvas.create_rectangle(60*self.x, 60*self.y, 60*self.x+60, 60*self.y+60, fill=self.colour)
-
-    def glow(self):
-        reference = {
-            "white":"red1",
-            "#3BD129":"red3"}
-        canvas.itemconfig(self.image, fill = reference[self.colour])
-
-    def unglow(self):
-        canvas.itemconfig(self.image, fill = self.colour)
 
 
 def populate_background(canvas):
-    for line in range(0,480,int(480/24)):
+    for line in range(40,480,int(480/24)):
+        colour_ref = {0:"grey", 20:"grey", 40:"black", 60:"grey"}
+        note_ref = {0:"e",20:"d",40:"c",60:"b",80:"a",100:"g",120:"f"}
+        octave_ref = {-1:"5",0:"4",1:"3",2:"2"}
         canvas.create_line(0,line,480,line,fill="grey")
-        canvas.create_line(line,0,line,480,fill="grey")
+        canvas.create_line(line,0,line,480,fill=colour_ref[line%80])
+        canvas.create_text(line+10,20,text=str(int(line/20)-1))
+        canvas.create_text(20,line+10,text=note_ref[line%140]+octave_ref[(line-60)//140])
+        print(line)
+#<140=0
+#140-280=1
+#280-420=2
+#420=3
+  
+#140, 140, 140
+def add_note(canvas, start, end, pitch):
+    note_ref={"c":0,"d":20,"e":40,"f":60,"g":80,"a":100,"b":120}
+    octave_ref={"2":0,"3":160,"4":320,"5":420}
+    canvas.create_rectangle(start*20+40, 480-note_ref[pitch[0]]-octave_ref[pitch[1]], end*20+60, 480-note_ref[pitch[0]]-octave_ref[pitch[1]]-20, fill="grey")
+
 
 
 window = tk.Tk()
@@ -33,4 +33,6 @@ window.minsize(480,480)
 canvas = tk.Canvas(window, width=480, height=480)
 canvas.pack()
 populate_background(canvas)
+for note in [[1,1,"e4"],[10,12,"c2"],[4,15,"c5"]]:
+    add_note(canvas,*note)
 window.mainloop()
--- Prototypes/hashing.py
+++ Prototypes/hashing.py
@@ -3,9 +3,9 @@ import bcrypt
 
 password = input("What should your password be: ")
 password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(14)).decode("utf-8")
-print(passord_hash)
-guess = input("Reenter your correct password")
-if bcrypt.checkpw(guess).encode('utf8'), password_hash.encode('utf8'):
+print(password_hash)
+guess = input("Reenter your correct password: ")
+if bcrypt.checkpw(guess.encode('utf8'), password_hash.encode('utf8')):
     print("Correct")
 else:
     print("Incorrect")

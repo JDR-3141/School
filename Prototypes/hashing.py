17e5dc4024d360508392c5b71d7a3fd8d52554d7
JDR-3141 <158481014+JDR-3141@users.noreply.github.com>
2024-03-22 09:52:52 +0000

    Create hashing.py

---
+++ Prototypes/hashing.py
@@ -0,0 +1,11 @@
+# https://codereview.stackexchange.com/questions/259710/login-system-using-bcrypt-and-mysql-to-be-used-for-any-future-projects
+import bcrypt
+
+password = input("What should your password be: ")
+password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(14)).decode("utf-8")
+print(passord_hash)
+guess = input("Reenter your correct password")
+if bcrypt.checkpw(guess).encode('utf8'), password_hash.encode('utf8'):
+    print("Correct")
+else:
+    print("Incorrect")

3ff86dd0be24830fc03e320a92929bcb07e6d985
JDR-3141 <158481014+JDR-3141@users.noreply.github.com>
2024-07-25 12:39:35 +0100

    edit

--- Algorithms/FFT.py
+++ Algorithms/FFT.py
@@ -2,6 +2,7 @@ import cmath
 from numpy import pi
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+import numpy as np
 
 
 
@@ -39,7 +40,7 @@ def change_format(frequency_bins, sample_frequency):
     N = len(frequency_bins)
     frequency_bins = frequency_bins[:N//2]
     frequency_resolution = sample_frequency/N
-    for i in range(len(frequency_bins)):
+    for i in range(N//2):
         frequency_bins[i] = [frequency_resolution*i, round(abs(frequency_bins[i])*2/N, 3)]
     
     return frequency_bins
@@ -53,11 +54,25 @@ def pad_to_power_of_two(complex_vector):
     next_pow2 = next_power_of_two(N)
     return complex_vector + [0] * (next_pow2 - N)
 
+def apply_window(segment, window):
+    return [s * w for s, w in zip(segment, window)]
+
+
+def STFT(data, window_size, hop_size, sample_frequency):
+    window = np.hanning(window_size)
+    stft_result = []
+    for start in range(0, len(data) - window_size, hop_size):
+        segment = data[start:start + window_size]
+        windowed_segment = apply_window(segment, window)
+        frequency_bins = FFT(windowed_segment)
+        formatted_bins = change_format(frequency_bins, sample_frequency)
+        stft_result.append(formatted_bins)
+    return stft_result
 
 samplerate, data = wavfile.read('test_audio.wav')
 samplerate, data1 = wavfile.read('test_audio1.wav')
 result_lt = [sum(i) for i in zip(data, data1)]  
-frequency_bins = FFT(list(result_lt))
+frequency_bins = STFT(list(result_lt), 1024, 512, samplerate)
 result = change_format(frequency_bins, samplerate)
 
 plt.plot(*zip(*result))

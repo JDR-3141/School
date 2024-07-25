39fee633e2578ec4730ae9b8cca02fc71194f7d3
JDR-3141 <158481014+JDR-3141@users.noreply.github.com>
2024-07-25 19:40:22 +0100

    edit

--- Algorithms/FFT&HPS.py
+++ Algorithms/FFT&HPS.py
@@ -9,7 +9,7 @@ HPS_number = 4
 
 def FFT (complex_vector):
     complex_vector = pad_to_power_of_two(complex_vector)
-    N = len (complex_vector)
+    N = complex_vector.size
 
     
 
@@ -24,10 +24,14 @@ def FFT (complex_vector):
         even.append( complex_vector [sample])
         odd.append( complex_vector [sample + 1])
 
+    even = np.array(even)
+    odd = np.array(odd)
+
     even_result = FFT (even)
     odd_result = FFT (odd)
 
     frequency_bins = [0] * N
+    frequency_bins = np.array(frequency_bins, dtype="complex_")
 
     for i in range(half_length):
 
@@ -41,13 +45,14 @@ def FFT (complex_vector):
 #     for i in range(HPS_number)
 
 def change_format(frequency_bins, sample_frequency):
-    N = len(frequency_bins)
+    N = frequency_bins.size
     frequency_bins = frequency_bins[:N//2]
     frequency_resolution = sample_frequency/N
+    frequency_bins_list = [0] * (N//2)
     for i in range(N//2):
-        frequency_bins[i] = [frequency_resolution*i, round(abs(frequency_bins[i])*2/N, 3)]
+        frequency_bins_list[i]  = [frequency_resolution*i, round(abs(frequency_bins[i])*2/N, 3)]
     
-    return frequency_bins
+    return frequency_bins_list
 
 
 def next_power_of_two(x):
@@ -56,30 +61,30 @@ def next_power_of_two(x):
 def pad_to_power_of_two(complex_vector):
     N = len(complex_vector)
     next_pow2 = next_power_of_two(N)
-    return complex_vector + [0] * (next_pow2 - N)
+    return np.pad(complex_vector, (0, next_pow2 - N), 'constant') if N < next_pow2 else complex_vector
 
-def apply_window(segment, window):
-    return [s * w for s, w in zip(segment, window)]
+# def apply_window(segment, window):
+#     return [s * w for s, w in zip(segment, window)]
 
 
 def STFT(data, window_size, hop_size, sample_frequency):
     window = np.hanning(window_size)
     stft_result = []
-    for start in range(0, len(data) - window_size, hop_size):
+    for start in range(0, data.size - window_size, hop_size):
         segment = data[start:start + window_size]
-        windowed_segment = apply_window(segment, window)
+        windowed_segment = np.multiply(segment, window)
         frequency_bins = FFT(windowed_segment)
         formatted_bins = change_format(frequency_bins, sample_frequency)
-        stft_result.append(formatted_bins)
+        stft_result.append(list(formatted_bins))
     return stft_result
 
 samplerate, data = wavfile.read('test_audio.wav')
 samplerate, data1 = wavfile.read('test_audio1.wav')
-result_lt = np.array(list(data) + list(data1))
+result_lt = np.concatenate((data, data1))
 wavfile.write('test_audio2.wav', samplerate, result_lt)
-result_lt = result_lt / np.max(np.abs(result_lt))
-frequency_bins = STFT(list(result_lt), 1024, 512, samplerate)
-
+#result_lt = result_lt / np.max(np.abs(result_lt))
+frequency_bins = STFT(result_lt, 1024, 512, samplerate)
+print(frequency_bins[0])
 plt.figure(figsize=(10, 6))
 for i, frame in enumerate(frequency_bins):
     plt.plot([f[0] for f in frame], [f[1] for f in frame], label=f'Frame {i}')
